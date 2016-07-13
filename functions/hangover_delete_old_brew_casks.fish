908e8d9523b45cb1e96605d46a0f4427a7651e52
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2016-07-13 12:24:24 +0200

    forcing delete: confirming is unaffordable

--- functions/hangover_delete_old_brew_casks.fish
+++ functions/hangover_delete_old_brew_casks.fish
@@ -8,8 +8,9 @@ function hangover_delete_old_brew_casks -d "Delete older-than-the-last-installed
 
   for cask in (ls | tr -d '/')
     for old_cask in (ls -t -1 $cask | tail -n +2 | tr -d '/')
-      echo "Deleting old cask $cask version $old_cask: (pwd)/$cask/$old_cask"
-      rm -r "$cask/$old_cask"
+      echo "Deleting old cask $cask version $old_cask: "(pwd)"/$cask/$old_cask"
+      sleep 2
+      rm -rf "$cask/$old_cask"
     end
   end
 
